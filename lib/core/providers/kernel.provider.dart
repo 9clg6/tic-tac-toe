44bf78825c0a19f45653d9c3119ec22fa6295766
Clement Guyon <clement.guyon@orkester.fr>
2025-11-17 14:10:01 +0100

    fix: kernel

--- lib/core/providers/kernel.provider.dart
+++ lib/core/providers/kernel.provider.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:tictactoe/core/providers/config/appconfig.provider.dart';
@@ -15,7 +16,13 @@ part 'kernel.provider.g.dart';
 Future<void> kernel(Ref ref) async {
   final AppConfig _ = ref.watch(appConfigProvider);
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } on Exception catch (e) {
+    debugPrint('Impossible to initialize Firebase: $e');
+  }
 
   ref.onDispose(() {
     ref
